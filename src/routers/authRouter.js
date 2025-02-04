0871f51d3f87ba66a3e296d6ec97fc816c78f533
SotaSill <sotasill@gmail.com>
2025-02-04 18:40:37 +0200

    login and auth logic

--- src/routers/authRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login, refresh } from '../controllers/userControllers.js';
-
-const router = express.Router();
-
-router.post('/login', login);
-router.post('/refresh', refresh);
-
-export default router; 
