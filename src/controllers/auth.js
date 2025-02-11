1cda5e03acaa5495ea33a777b9e964ab1c53c4cf
kaktuz <120398316+evgenyaroviy@users.noreply.github.com>
2025-02-11 20:09:37 +0200

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -10,42 +10,16 @@ import { refreshTokenLifetime } from '../constants/users.js';
 import { setupSession } from '../utils/setupSession.js';
 
 export const registerUserController = async (req, res) => {
-  try {
-    const user = await registerUser(req.body);
+  const user = await registerUser(req.body);
 
-    const loginPayload = {
-      email: req.body.email,
-      password: req.body.password,
-    };
-
-    const session = await loginUser(loginPayload);
-
-    res.cookie('refreshToken', session.refreshToken, {
-      httpOnly: true,
-      expires: new Date(Date.now() + refreshTokenLifetime),
-    });
-    res.cookie('sessionId', session._id, {
-      httpOnly: true,
-      expires: new Date(Date.now() + refreshTokenLifetime),
-    });
-
-    res.status(201).json({
-      status: 201,
-      message: 'Successfully registered and logged in a user!',
-      data: {
-        userId: user._id,
-        email: user.email,
-        accessToken: session.accessToken,
-        refreshToken: session.refreshToken,
-      },
-    });
-  } catch (error) {
-    console.error('Register user error:', error);
-    res.status(error.status || 500).json({
-      status: error.status || 500,
-      message: error.message,
-    });
-  }
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data: {
+      id: user._id,
+      email: user.email,
+    }
+  });
 };
 
 export const loginUserController = async (req, res) => {
