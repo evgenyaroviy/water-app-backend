dec21ede63df16e6bc07485a43bbf8b762ecf878
kaktuz <120398316+evgenyaroviy@users.noreply.github.com>
2025-02-11 21:33:02 +0200

    update auth and docs

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -36,7 +36,7 @@ export const registerUserController = async (req, res) => {
         id: user._id,
         email: user.email,
         accessToken: session.accessToken,
-        refreshToken: session.refreshToken,
+        // refreshToken: session.refreshToken,
       },
     });
   } catch (error) {
